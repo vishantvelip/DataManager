169c905429be06c21977a9311a5142042019cdaf
Vishantvelip <vishantvelip92@gmail.com>
2025-06-16 11:17:45 +0530

    Update multerConfig.js

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -13,7 +13,8 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    const uniqueName = `${Date.now()}${Math.round(Math.random() * 1e9)}${path.extname(file.originalname)}`;
+    cb(null, uniqueName);
   },
 });
 
