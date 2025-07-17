3a7c9c0bf76fef57e24337b9f33a61048d611b34
vishant009 <webdeveloper10980@gmail.com>
2025-07-17 15:40:31 +0530

    Update email.js

--- routes/email.js
+++ routes/email.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const emailjs = require('@emailjs/nodejs');
 
-
 emailjs.init({
   publicKey: 'JCxc_yKuAex9-h3QS', 
   privateKey: 'W_zMXHKXKvHjG9dwi63i-',
-
 });
 
 router.post('/send-email', async (req, res) => {
@@ -22,7 +20,6 @@ router.post('/send-email', async (req, res) => {
       email,
       message,
     };
-
 
     await emailjs.send('service_jzre4ps', 'template_9g5qb7p', templateParams);
 
