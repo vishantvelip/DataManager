5b4c6c13301a591d5db56939ba2ef355c1494edb
vishant009 <webdeveloper10980@gmail.com>
2025-07-17 15:25:03 +0530

    Update Skills.js

--- models/Skills.js
+++ models/Skills.js
@@ -1,11 +1,10 @@
-
 const { Schema, model } = require("mongoose");
 
 const skillSchema = new Schema(
   {
     skillName: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
-    projectImg: { type: String, default: "" }
+    projectImg: { type: String, default: "" },
   },
   { timestamps: true }
 );
